296cd36b5d771f68d802cd7e12508164bf3b2a69
cwirl <cwilmer@uninorte.edu.co>
2020-02-24 04:41:17 -0700

    feat: responsive home section

--- src/landpage/home/Home.js
+++ src/landpage/home/Home.js
@@ -6,6 +6,15 @@ export default function Home() {
   return (
     <section className="home">
       <div className="navbar-wrapper"><Navbar theme={"light"} /></div>
+      <div className="home-container">
+        <h1 className="title">Wilmer Cantillo</h1>
+        <p>
+          Electronics and self-taught software engineer. 
+          In my learning proccess i have developed skills 
+          to work on technology environment  and digital 
+          transformation.
+        </p>
+      </div>
     </section>
   )
 }
