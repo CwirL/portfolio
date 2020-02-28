32b216bd8ba1677a3f7ed609d79c42ec59212a32
cwirl <cwilmer@uninorte.edu.co>
2020-02-27 18:14:03 -0700

    test

---
+++ build/precache-manifest.9d24bc653d64b93d6a638f4c3dc09590.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56cd6154839d2d0f7abbeec27c1352d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "69397a6fe9a7921539a8",
+    "url": "/portfolio/static/css/main.8ef15979.chunk.css"
+  },
+  {
+    "revision": "e00373019b750bc05f7c",
+    "url": "/portfolio/static/js/2.9e471cb0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.9e471cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69397a6fe9a7921539a8",
+    "url": "/portfolio/static/js/main.1ebafa76.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "81cc224b6c5f572604d31566fbab6c0a",
+    "url": "/portfolio/static/media/Gravity-Bold.81cc224b.otf"
+  },
+  {
+    "revision": "ddf684cb238922ddf74d3082cd7d6b31",
+    "url": "/portfolio/static/media/Gravity-BoldItalic.ddf684cb.otf"
+  },
+  {
+    "revision": "663252db66bc724725e629859ee85b76",
+    "url": "/portfolio/static/media/Gravity-Italic.663252db.otf"
+  },
+  {
+    "revision": "5ec8b8fe67a2d233f4fa916b4dcea9d2",
+    "url": "/portfolio/static/media/Gravity-Light.5ec8b8fe.otf"
+  },
+  {
+    "revision": "0d824680c9767f6c0a1b7f77996cca12",
+    "url": "/portfolio/static/media/Gravity-LightItalic.0d824680.otf"
+  },
+  {
+    "revision": "c5833b37f54a60b621af207ee1748141",
+    "url": "/portfolio/static/media/Gravity-Regular.c5833b37.otf"
+  },
+  {
+    "revision": "3ae2ac75772700688806f6976444ff2f",
+    "url": "/portfolio/static/media/background.3ae2ac75.jpeg"
+  },
+  {
+    "revision": "a3bc1c3eaf652530dbf0222335743e81",
+    "url": "/portfolio/static/media/email-icon.a3bc1c3e.svg"
+  },
+  {
+    "revision": "e46b437c6c137d902936ad452ac91a63",
+    "url": "/portfolio/static/media/github-icon.e46b437c.svg"
+  },
+  {
+    "revision": "9506f84c9799845028ab3fa9a7d0f00f",
+    "url": "/portfolio/static/media/graduate-regular.9506f84c.ttf"
+  },
+  {
+    "revision": "6a8efaa5dc87ee86417bb10cf0be5515",
+    "url": "/portfolio/static/media/link-dark-icon.6a8efaa5.svg"
+  },
+  {
+    "revision": "6624a44357f333bd861261928cbf270b",
+    "url": "/portfolio/static/media/link-icon.6624a443.svg"
+  },
+  {
+    "revision": "e3c9f9d455c0738650eda5c746e0969d",
+    "url": "/portfolio/static/media/linkedin-icon.e3c9f9d4.svg"
+  },
+  {
+    "revision": "1b372551d593410f4db269097ce2824c",
+    "url": "/portfolio/static/media/me.1b372551.png"
+  },
+  {
+    "revision": "f09b57f00a7ed14de9b1cc6188dd642b",
+    "url": "/portfolio/static/media/menu-dark-icon.f09b57f0.svg"
+  },
+  {
+    "revision": "f6733cd730f2aff99b682836be0d0be1",
+    "url": "/portfolio/static/media/menu-light-icon.f6733cd7.svg"
+  }
+]);
